a8b2857bff05c0c165a0ea2137c77a8d9f6da6c2
NikitaKS <pancxxx2@gmail.com>
2020-04-25 10:01:08 +0300

    Updates

---
+++ precache-manifest.304ae8c7b4cec475ddfdd8d93fad49b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d863ebc42ab9dd2912f931492053acd",
+    "url": "/qr-code-rms-med/index.html"
+  },
+  {
+    "revision": "7fc88bc6b4cc8b8ceeed",
+    "url": "/qr-code-rms-med/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "2f2d38a04c23cf9e8720",
+    "url": "/qr-code-rms-med/static/css/main.e3f6838d.chunk.css"
+  },
+  {
+    "revision": "7fc88bc6b4cc8b8ceeed",
+    "url": "/qr-code-rms-med/static/js/2.5907d384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/qr-code-rms-med/static/js/2.5907d384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2d38a04c23cf9e8720",
+    "url": "/qr-code-rms-med/static/js/main.b2c02219.chunk.js"
+  },
+  {
+    "revision": "bb35defa8360806c0202",
+    "url": "/qr-code-rms-med/static/js/runtime-main.1f2135e9.js"
+  },
+  {
+    "revision": "33c3d2379ce675e71af2f57057b57b0a",
+    "url": "/qr-code-rms-med/static/media/logo.33c3d237.png"
+  },
+  {
+    "revision": "2508df2055ae3e150ddc505e107f95e7",
+    "url": "/qr-code-rms-med/static/media/question.2508df20.svg"
+  }
+]);
